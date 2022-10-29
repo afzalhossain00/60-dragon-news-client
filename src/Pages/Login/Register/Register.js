1a7f7b20fb3382bbebe960c6e7c84783bdc8469d
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-10-30 00:26:28 +0600

    terms and condition page

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const Register = () => {
     const [error, setError] = useState('')
-    const { createUser } = useContext(AuthContext);
+    const [accepted, setAccepted] = useState(false)
+    const { createUser, updateUserProfile } = useContext(AuthContext);
 
     const handleSubmit = event => {
         event.preventDefault();
@@ -23,11 +25,26 @@ const Register = () => {
                 console.log(user);
                 setError('')
                 form.reset()
+                handleUpdateUserProfile(name, photoURL)
             })
             .catch(error => {
                 console.error(error)
                 setError(error.message)
             })
+    }
+
+    const handleUpdateUserProfile = (name, photoURL) => {
+        const profile = {
+            displayName: name,
+            photoURL: photoURL
+        }
+        updateUserProfile(profile)
+            .then(() => { })
+            .catch((error) => console.error(error));
+    }
+
+    const handleAccepted = event => {
+        setAccepted(event.target.checked);
     }
 
 
@@ -49,7 +66,13 @@ const Register = () => {
                 <Form.Label>Password</Form.Label>
                 <Form.Control name='password' type="password" placeholder="Password" required />
             </Form.Group>
-            <Button variant="primary" type="submit">
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check
+                    type="checkbox"
+                    onClick={handleAccepted}
+                    label={<>Accept <Link to='/terms'>Terms and conditions</Link></>} />
+            </Form.Group>
+            <Button variant="primary" type="submit" disabled={!accepted}>
                 Register
             </Button> <br />
             <Form.Text className="text-danger">
